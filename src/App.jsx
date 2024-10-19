69009a35b5945f902ea9c3d7f925bb6efb0c3b8e
Brayancrack133 <brayanjj.ch@gmail.com>
2024-10-19 09:58:30 -0400

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import Card from "./components/Card";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [card, setCard] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await getCard("")
+        setCard(response);
 
+        
+      } catch (error) {
+        console.log("No pudimos solicitar las tarjetas");
+      }
+      
+    }
+  fetchData();
+   
+  }, [])
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        card.map(card=><Card card={card}/>)
+      }
     </>
   )
 }
 
-export default App
+export default App;
